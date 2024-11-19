91bd5f0d562299715acc70d00d56dc120736da4a
shailendrabhandari <shailendra.vandari@gmail.com>
2024-11-19 08:55:48 +0100

    more_updates

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='gan_package',
+    name='GANsforVirtualEye',
     version='0.1.0',
     author='Shailendra Bhandari',
     author_email='shailendra.bhandari@oslomet.no',
-    description='GAN for Time Series Generation',
+    description='This package provides an implementation of Generative Adversarial Networks (GANs) for time series generation, with flexible architecture options. Users can select different combinations of generator and discriminator models, including Convolutional Neural Networks (CNN) and Long Short-Term Memory networks (LSTM), to suit their specific needs.',
     packages=find_packages(),
     install_requires=[
         'numpy',
